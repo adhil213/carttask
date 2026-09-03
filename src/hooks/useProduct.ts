8984a46b0066f73bb488db99fd549d1a2c387bc2
M Adhil <madhil0770@gmail.com>
2026-09-03 16:23:37 +0530

    style: introduce global design system typography

--- src/hooks/useProduct.ts
+++ src/hooks/useProduct.ts
@@ -1,22 +1,22 @@
-import {useQuery} from '@tanstack/react-query'
+import { useQuery } from '@tanstack/react-query'
 import { productsResponseSchema } from '../schemas/productSchema'
 
 const PRODUCTS_URL = 'https://dummyjson.com/products'
 
-async function fetchProduct(){
-  const response=await fetch(PRODUCTS_URL)
+async function fetchProducts() {
+  const response = await fetch(PRODUCTS_URL)
 
-  if(!response.ok){
+  if (!response.ok) {
     throw new Error('Failed to fetch products')
   }
+
   const data: unknown = await response.json()
   return productsResponseSchema.parse(data)
 }
 
-export function useProducts(){
-    return useQuery({
-        queryKey:['products'],
-        queryFn:fetchProduct,
-    })
+export function useProducts() {
+  return useQuery({
+    queryKey: ['products'],
+    queryFn: fetchProducts,
+  })
 }
-
