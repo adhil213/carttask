6114c1f4409c216f7f0ab612a0fd58e73018d84c
M Adhil <madhil0770@gmail.com>
2026-09-03 15:27:45 +0530

    feat: add react-query hook to fetch products

---
+++ src/hooks/useProduct.ts
@@ -0,0 +1,22 @@
+import {useQuery} from '@tanstack/react-query'
+import { productsResponseSchema } from '../schemas/productSchema'
+
+const PRODUCTS_URL = 'https://dummyjson.com/products'
+
+async function fetchProduct(){
+  const response=await fetch(PRODUCTS_URL)
+
+  if(!response.ok){
+    throw new Error('Failed to fetch products')
+  }
+  const data: unknown = await response.json()
+  return productsResponseSchema.parse(data)
+}
+
+export function useProducts(){
+    return useQuery({
+        queryKey:['products'],
+        queryFn:fetchProduct,
+    })
+}
+
